4eda4bcca1233c0ae549622d94952e7bf13135a5
Brandon Olckers <43698232+SemcoDynamics@users.noreply.github.com>
2024-11-18 19:38:47 +0200

    updated checkout tests with expects

--- tests/Checkout.spec.ts
+++ tests/Checkout.spec.ts
@@ -1,6 +1,7 @@
 import {test} from "../Fixtures/LoginAndNavigate";
 import { expect } from "playwright/test";
 import { Checkout_Page } from "../POM/Checkout_Page";
+import { YourCart_Page } from "../POM/YourCart_Page";
 test.describe('Checkout tests', () => {
     test('Fill in form', async ({ loginAndNavigate, cartNavigate, page }) => {
         const checkoutPage = new Checkout_Page(page);
@@ -9,5 +10,15 @@ test.describe('Checkout tests', () => {
         await checkoutPage.formpostalCode.fill('2983')
         await checkoutPage.continueButton.click()
     })
+    test('fill form and cancel', async ({ loginAndNavigate, cartNavigate, page }) => {
+        const checkoutPage = new Checkout_Page(page);
+        const cartPage = new YourCart_Page(page)
+        await checkoutPage.formFirstName.fill('Jack');
+        await checkoutPage.formLastName.fill('Crabs');
+        await checkoutPage.formpostalCode.fill('2983')
+        await checkoutPage.cancelButton.click()
+        await expect(cartPage.inventoryItem).toContainText('Sauce Labs Bolt T-Shirt');
+        await expect(cartPage.inventoryItem).toContainText('$15.99')
+    });
     
 })
