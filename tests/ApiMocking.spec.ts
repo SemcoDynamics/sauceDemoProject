8fe6fe01982889af023bcf51bf8fc472041435ab
Brandon Olckers <43698232+SemcoDynamics@users.noreply.github.com>
2024-11-24 10:55:23 +0200

    Updated APi tests

--- tests/ApiMocking.spec.ts
+++ tests/ApiMocking.spec.ts
@@ -20,6 +20,9 @@ test.describe('Mock test', () => {
     test('My first mocked test', async ({ page }) => {
         await page.goto('https://conduit.bondaracademy.com/')
         await page.waitForSelector('.tag-list')
+        const taglist = page.locator('.tag-list')
+        const taglistText = await taglist.textContent()
+        expect(taglistText).toContain('Dell');
     
     });
      
@@ -53,5 +56,33 @@ test.describe('Mock test 2', () => {
         await page.goto('https://conduit.bondaracademy.com/')
         await page.waitForSelector('.article-preview')
     });
+    
+});
+test.describe('Mock API without a Json', () => {
+    test('Mock 1 - Fetching API and editing value', async ({ page }) => {
+        
+        await page.route('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', async route => {
+            const response = await route.fetch();
+            const responseBody = await response.json();
+            responseBody.article[0].favoritesCount = 0
+            await route.fulfill({
+                response,
+                //Parses test data json
+                body: JSON.stringify(responseBody)
+            })
+        })
+        await page.goto('https://conduit.bondaracademy.com/')
+        await page.locator('.tag-list').click()
+    });
+
+});
+
+test('Abort request', async ({ page }) => {
+    await page.goto('https://conduit.bondaracademy.com/');
+
+    await page.route('https://conduit-api.bondaracademy.com/api/tags', route => route.abort());
+    await page.route('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', route => route.abort());
+
+    await page.waitForTimeout(5000);
 });
 
