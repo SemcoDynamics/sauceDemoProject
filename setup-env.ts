157aa55ddee424cb86d5fa1a5e7c9880ba72958e
Brandon Olckers <43698232+SemcoDynamics@users.noreply.github.com>
2024-11-26 07:47:48 +0200

    Update yml

--- setup-env.ts
+++ setup-env.ts
@@ -2,3 +2,6 @@
 import { config } from 'dotenv';
 
 config(); // This loads variables from .env into process.env
+// Log to verify the variables are loaded
+console.log('ARGOS_TOKEN:', process.env.ARGOS_TOKEN);
+
