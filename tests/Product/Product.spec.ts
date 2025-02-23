509e30d210b284a2b1dd9297e560edf41a873ac1
Brandon Olckers <bolckers19@gmail.com>
2025-02-23 18:28:38 +0200

    Reverted back

--- tests/Product/Product.spec.ts
+++ tests/Product/Product.spec.ts
@@ -2,7 +2,6 @@ import { test } from '../../Fixtures/LoginAndNavigate';
 import { Login_Page } from '../../POM/Login_Page';
 import data from '../../HelperFiles/data.json';
 import { Product_Page } from '../../POM/Product_Page';
-import { Helper } from '../../HelperFiles/Helper';
 import { expect } from 'playwright/test';
 
 test.describe('Filter selection', () => {
@@ -14,46 +13,38 @@ test.describe('Filter selection', () => {
   
     test('Verify filter order (A to Z)', async ({ page }) => {
       const productPage = new Product_Page(page)
-      const helpers = new Helper(page);
 
       await productPage.selectFilter(data.productFilters.NameAtoZ.filterValue)
       const inventoryItemArray = await productPage.inventoryItemName.allTextContents()
 
        expect(inventoryItemArray).toEqual(data.productFilters.NameAtoZ.filterArray)
-       await helpers.screenShotPage("filter Z to A.png", 0.12)
     })
     test('Verify filter order Name (Z to A)', async ({ page }) => {
       const productPage = new Product_Page(page)
-      const helpers = new Helper(page);
 
       await productPage.selectFilter(data.productFilters.NameZtoA.filterValue)
       const inventoryItemArray = await productPage.inventoryItemName.allTextContents()
 
        expect(inventoryItemArray).toEqual(data.productFilters.NameZtoA.filterArray)
-       await helpers.screenShotPage("filter Z to A.png", 0.12)
     })
     test('Verify filter order Price (low to high)', async ({ page }) => {
       const productPage = new Product_Page(page)
-      const helpers = new Helper(page);
 
       await productPage.selectFilter(data.productFilters.PriceLowToHigh.filterValue)
       const inventoryItemArray = await productPage.inventoryItemName.allTextContents()
       console.log(inventoryItemArray)
 
        expect(inventoryItemArray).toEqual(data.productFilters.PriceLowToHigh.filterArray)
-       await helpers.screenShotPage("filter low to high.png", 0.02)
 
     })
     test('Verify filter order Price (high to low)', async ({ page }) => {
       const productPage = new Product_Page(page)
-      const helpers = new Helper(page);
 
       await productPage.selectFilter(data.productFilters.PriceHighToLow.filterValue)
       const inventoryItemArray = await productPage.inventoryItemName.allTextContents()
       console.log(inventoryItemArray)
 
        expect(inventoryItemArray).toEqual(data.productFilters.PriceHighToLow.filterArray)
-       await helpers.screenShotPage("filter high to low.png", 0.02)
 
     })
 
@@ -62,30 +53,24 @@ test.describe('Add Product to cart', () => {
   test('Add a product to cart', async ({loginAndNavigate, page }) => {
       await loginAndNavigate;
       const productPage = new Product_Page(page);
-      const helpers = new Helper(page);
 
       await productPage.selectProductHeaderAndAddToCart([data.itemDescriptionName.SauceLabsBoltTShirt]);
-      await helpers.screenShotPage("Single products added to cart.png", 0.02)
   });
   test('Add a muliple product to cart', async ({loginAndNavigate, page }) => {
       await loginAndNavigate;
       const productPage = new Product_Page(page);
-      const helpers = new Helper(page);
 
       await productPage.selectProductHeaderAndAddToCart([data.itemDescriptionName.SauceLabsBoltTShirt, data.itemDescriptionName.SauceLabsBikeLight])
-      await helpers.screenShotPage("Multiple products added to cart.png", 0.02)
     })
 })
 test.describe('View product description', () => {
   test('Select product description', async ({loginAndNavigate, page }) => {
     await loginAndNavigate;
     const productPage = new Product_Page(page);
-    const helpers = new Helper(page);
 
     await productPage.inventoryItemName.filter({hasText: data.itemDescriptionName.SauceLabsBikeLight}).click()
     await expect(productPage.inventoryItemName).toHaveText(data.itemDescriptionName.SauceLabsBikeLight)
     await expect(productPage.productDescriptionBody).toHaveText(data.productDescriptionBodyText.BikeLight)
-    await helpers.screenShotPage("Correct body.png", 0.02)
   });
   test('Add to cart from product description page', async ({loginAndNavigate, page}) => {
     await loginAndNavigate;
